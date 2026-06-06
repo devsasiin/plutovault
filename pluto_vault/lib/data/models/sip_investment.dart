db6775660f207d5559bd3332f1fb5f58285d62d8
Sasikumar Tamilselvan <sasikumar.tamilselvan@texmocastings.com>
2026-06-06 13:47:30 +0100

    Created sip data model

---
+++ pluto_vault/lib/data/models/sip_investment.dart
@@ -0,0 +1,11 @@
+class SipInvestment {
+  final String fundName;
+  final double amount;
+  final DateTime startDate;
+
+  SipInvestment({
+    required this.fundName,
+    required this.amount,
+    required this.startDate,
+  });
+}
