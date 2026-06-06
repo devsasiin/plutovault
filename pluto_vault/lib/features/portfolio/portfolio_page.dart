fb90c2513acae3ff4f5ae9dccdaf33fd8d526d6f
Sasikumar Tamilselvan <sasikumar.tamilselvan@texmocastings.com>
2026-06-06 13:59:40 +0100

    Created portfolio page

---
+++ pluto_vault/lib/features/portfolio/portfolio_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PortfolioPage extends StatelessWidget {
+  const PortfolioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Portfolio'),
+      ),
+      body: const Center(
+        child: Text('No Holdings Yet'),
+      ),
+    );
+  }
+}
