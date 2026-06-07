3bf982774ab0af08d21a339ca3c48cf0c32f354c
Sasikumar Tamilselvan <sasikumar.tamilselvan@texmocastings.com>
2026-06-07 08:50:03 +0100

    Implemented vault file save and load

--- pluto_vault/lib/features/dashboard/dashboard_page.dart
+++ pluto_vault/lib/features/dashboard/dashboard_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import '../portfolio/portfolio_page.dart';
+import 'dart:convert';
 
+import '../../core/encryption/encryption_service.dart';
+import '../../core/storage/vault_storage_service.dart';
+import '../../data/models/vault_data.dart';
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
@@ -30,6 +34,53 @@ class DashboardPage extends StatelessWidget {
       ElevatedButton(
         onPressed: () {},
         child: const Text('Backup'),
+      ),
+      ElevatedButton(
+        onPressed: () async {
+
+          final vault = VaultData(
+            portfolios: [],
+            sips: [],
+          );
+
+          final jsonData =
+              jsonEncode(vault.toJson());
+
+          final encrypted =
+              EncryptionService().encrypt(jsonData);
+
+          await VaultStorageService()
+              .saveVault(encrypted);
+
+          if (context.mounted) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(
+              const SnackBar(
+                content: Text('Vault Saved'),
+              ),
+            );
+          }
+        },
+        child: const Text('Create Vault'),
+      ),
+      ElevatedButton(
+        onPressed: () async {
+
+          final encrypted =
+              await VaultStorageService()
+                  .loadVault();
+
+          if (encrypted == null) {
+            return;
+          }
+
+          final jsonString =
+              EncryptionService()
+                  .decrypt(encrypted);
+
+          debugPrint(jsonString);
+        },
+        child: const Text('Load Vault'),
       ),
     ],
   ),
