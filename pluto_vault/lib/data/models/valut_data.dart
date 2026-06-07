f7306376589ca846f81208cd710d459d575f6e8a
Sasikumar Tamilselvan <sasikumar.tamilselvan@texmocastings.com>
2026-06-07 08:29:53 +0100

    added json support in vault data

--- pluto_vault/lib/data/models/valut_data.dart
+++ pluto_vault/lib/data/models/valut_data.dart
@@ -6,4 +6,18 @@ class VaultData {
     required this.portfolios,
     required this.sips,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'portfolios': portfolios,
+      'sips': sips,
+    };
+  }
+
+  factory VaultData.fromJson(Map<String, dynamic> json) {
+    return VaultData(
+      portfolios: json['portfolios'] ?? [],
+      sips: json['sips'] ?? [],
+    );
+  }
 }
